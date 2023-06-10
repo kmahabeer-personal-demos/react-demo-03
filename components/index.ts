fbd153494d9b736670310990edff4f50dd693d0a
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2023-06-09 23:33:21 -0400

    add footer to layout

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Hero from './Hero';
 import Button from './Button';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
-export { Hero, Button, Navbar };
+export { Hero, Button, Navbar, Footer };
