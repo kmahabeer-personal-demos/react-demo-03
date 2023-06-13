8c0e5316623e81731102b8ff89bf5433baeda3e6
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2023-06-12 23:39:13 -0400

    add searchbar functionality

--- types/index.ts
+++ types/index.ts
@@ -35,3 +35,11 @@ export interface CarDetailsProps {
 	closeModal: () => void;
 	car: CarProps;
 }
+
+export interface FilterProps {
+	manufacturer?: string;
+	year?: number;
+	model?: string;
+	limit?: number;
+	fuel?: string;
+}
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,13 @@
-export async function fetchCars() {
+import { FilterProps } from '@/types';
+
+export async function fetchCars(filters: FilterProps) {
+	const { manufacturer, year, model, limit, fuel } = filters;
 	const headers = {
 		'X-RapidAPI-Key': '27d5220103mshb43523a2e0ac30cp1906e1jsnce4b6d352250',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
 	};
 	const response = await fetch(
-		'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
 		{ headers: headers }
 	);
 
