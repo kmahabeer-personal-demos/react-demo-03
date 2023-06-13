60ca607616ae09e910802991b54d0d13cb337918
Kevin Mahabeer <11927611+kmahabeer@users.noreply.github.com>
2023-06-12 20:21:54 -0400

    add car card component

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 import Image from 'next/image';
 import { ButtonProps } from '@/types';
 
-const Button = ({ title, containerStyles, handleClick, btnType }: ButtonProps) => {
+const Button = ({
+	title,
+	containerStyles,
+	handleClick,
+	btnType,
+	textStyles,
+	rightIcon,
+	isDisabled,
+}: ButtonProps) => {
 	return (
 		<button
 			disabled={false}
@@ -11,7 +19,17 @@ const Button = ({ title, containerStyles, handleClick, btnType }: ButtonProps) =
 			className={`custom-btn ${containerStyles}`}
 			onClick={handleClick}
 		>
-			<span className={`flex-1`}>{title}</span>
+			<span className={`flex-1 ${textStyles}`}>{title}</span>
+			{rightIcon && (
+				<div className='relative w-6 h-6'>
+					<Image
+						src={rightIcon}
+						alt='right icon'
+						fill
+						className='object-contain'
+					/>
+				</div>
+			)}
 		</button>
 	);
 };
--- components/index.ts
+++ components/index.ts
@@ -4,5 +4,6 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Searchbar from './Searchbar';
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
 
-export { Hero, Button, Navbar, Footer, Searchbar, SearchManufacturer };
+export { Hero, Button, Navbar, Footer, Searchbar, SearchManufacturer, CarCard };
